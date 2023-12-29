9345f855851344090ac9bf7aec3631509f7b8d9d
Sina Heidari <sina.heidaripour@gmail.com>
2023-12-29 22:12:50 +0330

    add ground control

--- src/farmopolis.cpp
+++ src/farmopolis.cpp
@@ -1,16 +1,17 @@
 #include "farmopolis.h"
 #include "ui_farmopolis.h"
 
-//graphics view
-#include <QGraphicsScene>
-#include <QGraphicsView>
-#include <QGraphicsRectItem>
-#include <QHBoxLayout>
-
 //file
 #include <QFile>
 #include <QTextStream>
 #include "QString"
+
+#include <QMouseEvent>
+
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
 
 Farmopolis::Farmopolis(QWidget *parent) :
     QMainWindow(parent),
@@ -31,9 +32,56 @@ Farmopolis::Farmopolis(QWidget *parent) :
         this->setStyleSheet(style);
     }
 
+    // Create a new QGridLayout and add it to the graphics view
+    QGridLayout *gridLayout = new QGridLayout(ui->groundwidget);
+    const int numRows = 4;
+    const int numCols = 5;
+    for (int i = 0; i < numRows; ++i) {
+        for (int j = 0; j < numCols; ++j) {
+            QLabel *square = new QLabel(ui->groundwidget);
+            gridLayout->addWidget(square, i, j);
+        }
+    }
+
+    QList<QLabel *> labelList = ui->groundwidget->findChildren<QLabel *>();
+    for (QLabel *label : labelList) {
+        label->installEventFilter(this);
+    }
 }
 
-Farmopolis::~Farmopolis()
-{
+Farmopolis::~Farmopolis() {
     delete ui;
 }
+
+bool Farmopolis::eventFilter(QObject *obj, QEvent *event) {
+    if (event->type() == QEvent::MouseButtonPress) {
+        QLabel *clickedLabel = qobject_cast<QLabel *>(obj);
+        if (clickedLabel) {
+            QString originalStyles = clickedLabel->styleSheet();
+            clickedLabel->setStyleSheet(originalStyles + "border:3px solid #212529;") ;
+            if(currentground) {
+                QString originalStyles = currentground->styleSheet();
+                currentground->setStyleSheet(originalStyles + "border:3px solid #FFB703;") ;
+            }
+            currentground = clickedLabel ;
+        }
+    }
+    return QObject::eventFilter(obj, event);
+}
+
+void Farmopolis::on_buygroundpushButton_clicked() {
+    if (currentground && !currentground->styleSheet().contains("#55A630")) {
+        currentground->setStyleSheet("background-color: #55A630; border:3px solid #212529;");
+    }
+}
+
+
+
+void Farmopolis::on_buyhenpushButton_clicked()
+{
+    if (currentground && currentground->styleSheet().contains("#55A630")) {
+        QString originalStyles = currentground->styleSheet();
+        currentground->setStyleSheet("background: #55A630 url(:/rec/Icons/farm/hen.png) no-repeat;");
+    }
+}
+
--- src/farmopolis.h
+++ src/farmopolis.h
@@ -3,6 +3,11 @@
 
 #include <QMainWindow>
 
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
+
 namespace Ui {
 class Farmopolis;
 }
@@ -14,9 +19,18 @@ class Farmopolis : public QMainWindow
 public:
     explicit Farmopolis(QWidget *parent = nullptr);
     ~Farmopolis();
+    void groundlabelClicked(QLabel *label) ;
+    bool eventFilter(QObject *obj, QEvent *event) ;
+private slots:
+
+    void on_buygroundpushButton_clicked();
+
+    void on_buyhenpushButton_clicked();
 
 private:
     Ui::Farmopolis *ui;
+    QLabel *currentground = nullptr;
+
 };
 
 #endif // FARMOPOLIS_H
